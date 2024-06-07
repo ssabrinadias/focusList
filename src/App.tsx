0329240943908aba72476d1ff48575ff72f5f579
Sabrina Dias <dias.sabrina@ifood.com.br>
2024-06-07 16:37:54 -0300

    feat: styled config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TodoList from "./ToDoList";
 
 function App() {
   return (
     <Router>
       <h1>To Do List</h1>
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route
+          path="/"
+          element={
+            <TodoList/>
+          }
+        />
       </Routes>
     </Router>
   );
