2163f0fbb46aeba949bcd1f612dae4d5f3b86c4f
Sabrina Dias <dias.sabrina@ifood.com.br>
2024-06-07 12:30:26 -0300

    feat: route config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+function App() {
   return (
-    <>
+    <Router>
       <h1>To Do List</h1>
-    </>
-  )
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
