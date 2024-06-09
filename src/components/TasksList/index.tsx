f4ff817285ef6679b427cf4573a66ae542fe131d
Sabrina Dias <dias.sabrina@ifood.com.br>
2024-06-08 23:44:35 -0300

    fix: format text

--- src/components/TasksList/index.tsx
+++ src/components/TasksList/index.tsx
@@ -4,7 +4,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
-import { makeStyles,Theme } from '@material-ui/core/styles';
+import { makeStyles, Theme } from '@material-ui/core/styles';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
@@ -31,24 +31,24 @@ function TasksList() {
   return (
     <List>
       {tasks?.map((task, id) => (
-          <ListItem key={task.id + id} button className={classes.root}>
-            <ListItemIcon>
-              <CheckCircleIcon color="primary" />
-            </ListItemIcon>
-            <ListItemText
-              primary={task.title}
-              secondary={task.description ? task.description : null}
-            />
-            <ListItemSecondaryAction>
-              <IconButton edge="end" aria-label="edit" onClick={() => console.log('edit')}>
-                <EditIcon />
-              </IconButton>
-              <IconButton edge="end" aria-label="delete" onClick={() => console.log('delete')}>
-                <DeleteIcon />
-              </IconButton>
-            </ListItemSecondaryAction>
-          </ListItem>
-        ))}
+        <ListItem key={task.id + id} button className={classes.root}>
+          <ListItemIcon>
+            <CheckCircleIcon color="primary" />
+          </ListItemIcon>
+          <ListItemText
+            primary={task.title}
+            secondary={task.description ? task.description : null}
+          />
+          <ListItemSecondaryAction>
+            <IconButton edge="end" aria-label="edit" onClick={() => console.log('edit')}>
+              <EditIcon />
+            </IconButton>
+            <IconButton edge="end" aria-label="delete" onClick={() => console.log('delete')}>
+              <DeleteIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
+      ))}
     </List>
   );
 }
